16214447cd1e2622e202969f400d75a8feae2c87
Shubham Jaiswal <sj805217@gmail.com>
2026-08-20 16:55:29 +0530

    Remove tracked Next.js build files

--- chatbot/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_1u3kqbn._.js","static/chunks/_181lc-r._.js","static/chunks/node_modules_next_dist_00pwe04._.js"],
-    source: "entry"
-});
